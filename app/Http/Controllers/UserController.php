408632e9c30750c6284278fedf5416dbef631689
Papkxo87 <papkoi@mail.ru>
2021-10-06 21:10:03 +0300

    LP-10 add update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Contact;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -10,31 +9,45 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = new User();
-        return view('users', ['data' => $user->all()]);
+        response()->json(User::all());
     }
 
     public function show($id)
     {
-        $user = new User();
-        return view('one-users', ['data' => $user->find($id)]);
+        response()->json(User::find($id));
     }
 
-    public function store(Request $req)
+    public function store(Request $request)
     {
         $user = new User();
-        $user->first_name = $req->input('first_name');
-        $user->last_name = $req->input('last_name');
-        $user->role = $req->input('role');
-        $user->email = $req->input('email');
-        $user->password = $req->input('password');
+        $user = $user::create([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'role' => $request->input('role'),
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+        ]);
 
-        $user->save();
-
+        response()->json($user);
     }
 
     public function delete($id)
     {
         User::find($id)->delete();
+    }
+
+    public function update($id, Request $request)
+    {
+        $user = new User();
+
+        $user = $user->update([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'role' => $request->input('role'),
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+        ]);
+
+        response()->json($user);
     }
 }
