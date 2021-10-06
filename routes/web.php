413c96be10b02b15c7112ffb0729fa52c5568453
Papkxo87 <papkoi@mail.ru>
2021-10-06 19:58:29 +0300

    LP-10 add routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,3 @@ Route::get('/', function () {
 })->name('welcome');
 
 
-Route::get('/users/all', [UserController::class, 'index'])->name('all-users');
-Route::get('/users/all/{id}', [UserController::class, 'show'])->name('id-users');
-Route::post('/users/store', [UserController::class, 'store'])->name('store-users');
-Route::delete('/users/all/{id}/delete', [UserController::class, 'delete'])->name('delete-users');
